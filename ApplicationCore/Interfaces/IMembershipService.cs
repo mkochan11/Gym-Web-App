94bd77d46ea9fb6004178caff42349bde6a45659
mkochan11 <kochanowski.michal.11@gmail.com>
2024-12-20 11:52:06 +0100

    feat: add membership service, get active membership method

---
+++ ApplicationCore/Interfaces/IMembershipService.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    internal interface IMembershipService
+    {
+        Task<GymMembership> GetActiveMembership(int clientId);
+    }
+}
---
+++ ApplicationCore/Services/MembershipService.cs
@@ -0,0 +1,53 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class MembershipService : IMembershipService
+    {
+        private readonly IRepository<GymMembership> _membershipRepository;
+
+        public MembershipService(IRepository<GymMembership> membershipRepository)
+        {
+            _membershipRepository = membershipRepository;
+        }
+
+        /// <summary>
+        /// Gets Client's active GymMembership
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns>
+        /// A <see cref="GymMembership"/> object representing the active membership of the client, 
+        /// or <c>null</c> if no active membership is found.
+        /// </returns>
+        public async Task<GymMembership> GetActiveMembership(int clientId)
+        {
+            var _membershipSpec = new FindMembershipByClientId(clientId);
+            var clientMemberships = await _membershipRepository.ListAsync(_membershipSpec);
+
+            GymMembership activeMembership = null;
+
+            foreach (var clientMembership in clientMemberships)
+            {
+                if (IsMembershipActive(clientMembership))
+                {
+                    activeMembership = clientMembership;
+                }
+            }
+            return activeMembership;
+        }
+
+
+        private bool IsMembershipActive(GymMembership membership) {
+            DateTime currentDate = DateTime.Now;
+
+            return membership.StartDate < currentDate && membership.EndDate > currentDate;
+        }
+    }
+}
